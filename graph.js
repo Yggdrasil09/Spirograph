282a7c5309e26488efbd8fd87ded026005274e0e
Yggdrasil09 <karthik99th.tnk@gamil.com>
2018-10-23 12:47:51 +0530

    A spirograph with attached slider

--- graph.js
+++ graph.js
@@ -1,28 +1,43 @@
 var x=300,y=300;
 var x1,y1;
-var ang=0;
 var x2,y2;
+
+var ang=0;
 var ang1=0;
+var path=[];
 
 function setup()
 {
     var canvas=createCanvas(600, 600);
     canvas.position((windowWidth-width)/2,(windowHeight-height)/2);
-    background(255);
+    background(0);
+    ang=float(angle1);
+    ang1=float(angle2);
+    path=[];
 }
 
 function draw()
 {
-    background(0);
-    strokeWeight(2);
+    ellipse(x,y,600,600);
     stroke(0);
-    ellipse(x,y,200,200);
-    x1=x+100*sin(ang);
-    y1=y-100*cos(ang);
-    ellipse(x1,y1,100,100);
+    strokeWeight(2);
+    x1=300+225*sin(ang);
+    y1=300-225*cos(ang);
+    ellipse(x1,y1,150,150);
+
     x2=x1+50*sin(ang1);
     y2=y1-50*cos(ang1);
+
     ellipse(x2,y2,50,50);
-    ang+=0.03;
-    ang1+=0.06;
+    path.push(createVector(x2,y2));
+    point(x1,y1);
+    strokeWeight(3);
+
+    ang+=0.06;
+    ang1+=0.1;
+
+    for(var i=0;i<path.length;i++)
+    {
+        point(path[i].x,path[i].y);
+    }
 }
