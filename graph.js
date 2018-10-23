7721956f3441611e0b8c1b6e53ae3a791e1e5f5c
Yggdrasil09 <karthik99th.tnk@gamil.com>
2018-10-23 16:10:58 +0530

    Made the spiral

--- graph.js
+++ graph.js
@@ -1,28 +1,34 @@
 var x=300,y=300;
+
 var x1,y1;
+
 var ang=0;
-var x2,y2;
 var ang1=0;
+
+var path=[];
 
 function setup()
 {
-    var canvas=createCanvas(600, 600);
+    var canvas=createCanvas(600,600);
     canvas.position((windowWidth-width)/2,(windowHeight-height)/2);
-    background(255);
+    background(0);
 }
 
 function draw()
 {
     background(0);
-    strokeWeight(2);
-    stroke(0);
-    ellipse(x,y,200,200);
-    x1=x+100*sin(ang);
-    y1=y-100*cos(ang);
+    stroke(255);
+    fill(0);
+    ellipse(x,y,300,300);
+    x1=x+100*cos(ang);
+    y1=y+100*sin(ang);
     ellipse(x1,y1,100,100);
-    x2=x1+50*sin(ang1);
-    y2=y1-50*cos(ang1);
-    ellipse(x2,y2,50,50);
-    ang+=0.03;
-    ang1+=0.06;
+    line(x1,y1,x1+150*cos(ang1),y1+150*sin(ang1));
+    path.push(createVector(x1+150*cos(ang1),y1+150*sin(ang1)));
+    ang+=0.02;
+    ang1+=0.03;
+    for(var i=0;i<path.length;i++)
+    {
+        point(path[i].x,path[i].y);
+    }
 }
